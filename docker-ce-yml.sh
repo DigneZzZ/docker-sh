5919ba27cabb976581eccd513b7bfc21d136a28e
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-03-16 23:30:48 +0300

    Update docker-ce-yml.sh

--- docker-ce-yml.sh
+++ docker-ce-yml.sh
@@ -58,4 +58,4 @@ else
 fi
 
 # Установка Docker Compose
-docker-composecurl -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
+curl -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
